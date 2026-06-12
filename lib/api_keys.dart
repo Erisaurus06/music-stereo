1128f6435ef90524edfd287da2f85c7443f5a1d0
Erik <coronelefa113@gmail.com>
2026-06-12 06:16:20 -0600

    proteccion de tokens y apikeys de ojo publico

--- lib/api_keys.dart
+++ lib/api_keys.dart
@@ -2,25 +2,16 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiKeys {
   // Llaves de Spotify (Dinobot Company)
-  static String get spotifyClientId =>
-      dotenv.env['SPOTIFY_CLIENT_ID'] ?? "538237e9cc984eed9704d0eab041a56d";
+  static String get spotifyClientId => dotenv.env['SPOTIFY_CLIENT_ID'] ?? "";
   static String get spotifyClientSecret =>
-      dotenv.env['SPOTIFY_CLIENT_SECRET'] ?? "46b4e4a0d8014227990a0f1bad6559f9";
+      dotenv.env['SPOTIFY_CLIENT_SECRET'] ?? "";
 
   // Llaves de Genius (Cerebro de Letras)
-  static String get geniusClientId =>
-      dotenv.env['GENIUS_CLIENT_ID'] ??
-      "qJ3tM8d9WSLt0j9Xl4tiwiVwbhKKh-YCNvVkgBFDkrO7-I60BOwbhUN0LANmfXG0";
+  static String get geniusClientId => dotenv.env['GENIUS_CLIENT_ID'] ?? "";
   static String get geniusClientSecret =>
-      dotenv.env['GENIUS_CLIENT_SECRET'] ??
-      "j1822A1aRLs3zoY7fkFzgtN1Itt2V7qnTpKvN7gLcgPD4g-6eu8hRRadUMtnreiuEkkK5zJlXbaHK69GgwvGAw";
-  static String get geniusToken =>
-      dotenv.env['GENIUS_TOKEN'] ??
-      "uJSvnurYq2ASqZxVyn1_mzYJ3rLk6-mW3eXmFu0Nxa8jKjLpUPGmEDRUJxlhxDN1";
+      dotenv.env['GENIUS_CLIENT_SECRET'] ?? "";
+  static String get geniusToken => dotenv.env['GENIUS_TOKEN'] ?? "";
 
-  static String get supabaseUrl =>
-      dotenv.env['SUPABASE_URL'] ?? "https://ublqvfmlqppvcpgqyuqh.supabase.co";
-  static String get supabaseAnonKey =>
-      dotenv.env['SUPABASE_ANON_KEY'] ??
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibHF2Zm1scXBwdmNwZ3F5dXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMjg0MTUsImV4cCI6MjA5MjYwNDQxNX0.sFyhv_T8LAaXwdMd8_RWPIT_cEzKDzz_9MMKB2I7EEI";
+  static String get supabaseUrl => dotenv.env['SUPABASE_URL'] ?? "";
+  static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? "";
 }
